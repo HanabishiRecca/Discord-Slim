d83bc45bd8907b5f67ec0a8b0318712909e24218
Hanabishi <13597663+HanabishiRecca@users.noreply.github.com>
2020-12-19 14:22:17 +0500

    More additional flags and sets

--- src/client.ts
+++ src/client.ts
@@ -16,8 +16,11 @@ export enum OPCode {
     DISPATCH = 0,
     HEARTBEAT = 1,
     IDENTIFY = 2,
+    PRESENCE_UPDATE = 3,
+    VOICE_STATE_UPDATE = 4,
     RESUME = 6,
     RECONNECT = 7,
+    REQUEST_GUILD_MEMBERS = 8,
     INVALID_SESSION = 9,
     HELLO = 10,
     HEARTBEAT_ACK = 11,
@@ -336,6 +339,7 @@ export const Routes = {
 };
 
 export enum Permissions {
+    NO_PERMISSIONS = 0,
     CREATE_INSTANT_INVITE = (1 << 0),
     KICK_MEMBERS = (1 << 1),
     BAN_MEMBERS = (1 << 2),
@@ -370,6 +374,7 @@ export enum Permissions {
 };
 
 export enum Intents {
+    SYSTEM_ONLY = 0,
     GUILDS = (1 << 0),
     GUILD_MEMBERS = (1 << 1),
     GUILD_BANS = (1 << 2),
@@ -386,6 +391,144 @@ export enum Intents {
     DIRECT_MESSAGE_REACTIONS = (1 << 13),
     DIRECT_MESSAGE_TYPING = (1 << 14),
 };
+
+export enum AuditLogEvents {
+    GUILD_UPDATE = 1,
+    CHANNEL_CREATE = 10,
+    CHANNEL_UPDATE = 11,
+    CHANNEL_DELETE = 12,
+    CHANNEL_OVERWRITE_CREATE = 13,
+    CHANNEL_OVERWRITE_UPDATE = 14,
+    CHANNEL_OVERWRITE_DELETE = 15,
+    MEMBER_KICK = 20,
+    MEMBER_PRUNE = 21,
+    MEMBER_BAN_ADD = 22,
+    MEMBER_BAN_REMOVE = 23,
+    MEMBER_UPDATE = 24,
+    MEMBER_ROLE_UPDATE = 25,
+    MEMBER_MOVE = 26,
+    MEMBER_DISCONNECT = 27,
+    BOT_ADD = 28,
+    ROLE_CREATE = 30,
+    ROLE_UPDATE = 31,
+    ROLE_DELETE = 32,
+    INVITE_CREATE = 40,
+    INVITE_UPDATE = 41,
+    INVITE_DELETE = 42,
+    WEBHOOK_CREATE = 50,
+    WEBHOOK_UPDATE = 51,
+    WEBHOOK_DELETE = 52,
+    EMOJI_CREATE = 60,
+    EMOJI_UPDATE = 61,
+    EMOJI_DELETE = 62,
+    MESSAGE_DELETE = 72,
+    MESSAGE_BULK_DELETE = 73,
+    MESSAGE_PIN = 74,
+    MESSAGE_UNPIN = 75,
+    INTEGRATION_CREATE = 80,
+    INTEGRATION_UPDATE = 81,
+    INTEGRATION_DELETE = 82,
+}
+
+export enum ChannelTypes {
+    GUILD_TEXT = 0,
+    DM = 1,
+    GUILD_VOICE = 2,
+    GROUP_DM = 3,
+    GUILD_CATEGORY = 4,
+    GUILD_NEWS = 5,
+    GUILD_STORE = 6,
+}
+
+export enum MessageTypes {
+    DEFAULT = 0,
+    RECIPIENT_ADD = 1,
+    RECIPIENT_REMOVE = 2,
+    CALL = 3,
+    CHANNEL_NAME_CHANGE = 4,
+    CHANNEL_ICON_CHANGE = 5,
+    CHANNEL_PINNED_MESSAGE = 6,
+    GUILD_MEMBER_JOIN = 7,
+    USER_PREMIUM_GUILD_SUBSCRIPTION = 8,
+    USER_PREMIUM_GUILD_SUBSCRIPTION_TIER_1 = 9,
+    USER_PREMIUM_GUILD_SUBSCRIPTION_TIER_2 = 10,
+    USER_PREMIUM_GUILD_SUBSCRIPTION_TIER_3 = 11,
+    CHANNEL_FOLLOW_ADD = 12,
+    GUILD_DISCOVERY_DISQUALIFIED = 14,
+    GUILD_DISCOVERY_REQUALIFIED = 15,
+    REPLY = 19,
+    APPLICATION_COMMAND = 20,
+}
+
+export enum MessageActivityTypes {
+    DEFAULT = 0,
+    RECIPIENT_ADD = 1,
+    RECIPIENT_REMOVE = 2,
+    CALL = 3,
+    CHANNEL_NAME_CHANGE = 4,
+    CHANNEL_ICON_CHANGE = 5,
+    CHANNEL_PINNED_MESSAGE = 6,
+    GUILD_MEMBER_JOIN = 7,
+    USER_PREMIUM_GUILD_SUBSCRIPTION = 8,
+    USER_PREMIUM_GUILD_SUBSCRIPTION_TIER_1 = 9,
+    USER_PREMIUM_GUILD_SUBSCRIPTION_TIER_2 = 10,
+    USER_PREMIUM_GUILD_SUBSCRIPTION_TIER_3 = 11,
+    CHANNEL_FOLLOW_ADD = 12,
+    GUILD_DISCOVERY_DISQUALIFIED = 14,
+    GUILD_DISCOVERY_REQUALIFIED = 15,
+    REPLY = 19,
+    APPLICATION_COMMAND = 20,
+}
+
+export enum MessageFlags {
+    NO_FLAGS = 0,
+    CROSSPOSTED = 1 << 0,
+    IS_CROSSPOST = 1 << 1,
+    SUPPRESS_EMBEDS = 1 << 2,
+    SOURCE_MESSAGE_DELETED = 1 << 3,
+    URGENT = 1 << 4,
+}
+
+export enum MessageStickerFormatTypes {
+    PNG = 1,
+    APNG = 2,
+    LOTTIE = 3,
+}
+
+export enum DefaultMessageNotificationLevel {
+    ALL_MESSAGES = 0,
+    ONLY_MENTIONS = 1,
+}
+
+export enum ExplicitContentFilterLevel {
+    DISABLED = 0,
+    MEMBERS_WITHOUT_ROLES = 1,
+    ALL_MEMBERS = 2,
+}
+
+export enum MFA_Level {
+    NONE = 0,
+    ELEVATED = 1,
+}
+
+export enum VerificationLevel {
+    NONE = 0,
+    LOW = 1,
+    MEDIUM = 2,
+    HIGH = 3,
+    VERY_HIGH = 4,
+}
+
+export enum PremiumTier {
+    NONE = 0,
+    TIER_1 = 1,
+    TIER_2 = 2,
+    TIER_3 = 3,
+}
+
+export enum TargetUserTypes {
+    STREAM = 1,
+}
 
 type RequestResult = {
     code: number;
