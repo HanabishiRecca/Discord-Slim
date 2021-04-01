9742cd4e840523ceb7866becde1c3ff294defbe2
Hanabishi <13597663+HanabishiRecca@users.noreply.github.com>
2021-04-02 01:37:53 +0500

    Unused OPCodes helper removed

--- src/helpers.ts
+++ src/helpers.ts
@@ -10,20 +10,6 @@ export enum TokenTypes {
     Bearer = 'Bearer',
 }
 
-export enum OPCodes {
-    DISPATCH = 0,
-    HEARTBEAT = 1,
-    IDENTIFY = 2,
-    PRESENCE_UPDATE = 3,
-    VOICE_STATE_UPDATE = 4,
-    RESUME = 6,
-    RECONNECT = 7,
-    REQUEST_GUILD_MEMBERS = 8,
-    INVALID_SESSION = 9,
-    HELLO = 10,
-    HEARTBEAT_ACK = 11,
-}
-
 export const Permissions = {
     NO_PERMISSIONS: 0x000000000n,
     CREATE_INSTANT_INVITE: 0x000000001n,
