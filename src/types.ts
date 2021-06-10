9e4dcae5efc870e4c92f3f0c8954949c0be1ba92
Hanabishi <13597663+HanabishiRecca@users.noreply.github.com>
2021-06-10 12:22:31 +0500

    Update ThreadMember type

--- src/types.ts
+++ src/types.ts
@@ -243,8 +243,8 @@ export type ThreadMetadata = {
 };
 
 export type ThreadMember = {
-    id: string;
-    user_id: string;
+    id?: string;
+    user_id?: string;
     join_timestamp: string;
     flags: number;
 };
