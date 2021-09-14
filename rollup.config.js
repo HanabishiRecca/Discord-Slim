42849316852737fcabea1d4f774eab4f3e53778d
Hanabishi <13597663+HanabishiRecca@users.noreply.github.com>
2021-09-14 20:13:03 +0500

    Improved build process

--- rollup.config.js
+++ rollup.config.js
@@ -1,26 +1,30 @@
-import typescript from '@rollup/plugin-typescript';
-import dts from 'rollup-plugin-dts';
+import ts from 'rollup-plugin-ts';
+import del from 'rollup-plugin-delete';
 
 const input = 'src/index.ts';
 
 const output = {
     dir: 'package/dist',
     format: 'es',
+    preserveModules: true,
+    preserveModulesRoot: 'src',
 };
 
-export default [
-    {
-        input,
-        output,
-        plugins: [
-            typescript(),
-        ],
-    },
-    {
-        input,
-        output,
-        plugins: [
-            dts(),
-        ],
-    },
+const exclude = [
+    'util.d.ts',
 ];
+
+const plugins = [
+    ts(),
+    del({ targets: `${output.dir}/*`, runOnce: true }),
+    del({ targets: exclude.map((e) => `${output.dir}/${e}`), hook: 'closeBundle' }),
+];
+
+const external = [
+    'ws',
+    'events',
+    'https',
+    'url',
+];
+
+export default { input, output, plugins, external };
