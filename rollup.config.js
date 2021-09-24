8259066394c62530551078aea1d104c87bda3407
Hanabishi <13597663+HanabishiRecca@users.noreply.github.com>
2021-09-24 17:02:59 +0500

    Make bundle with updated 'rollup-plugin-ts'

--- rollup.config.js
+++ rollup.config.js
@@ -6,18 +6,11 @@ const input = 'src/index.ts';
 const output = {
     dir: 'package/dist',
     format: 'es',
-    preserveModules: true,
-    preserveModulesRoot: 'src',
 };
-
-const exclude = [
-    'util.d.ts',
-];
 
 const plugins = [
     ts(),
     del({ targets: `${output.dir}/*`, runOnce: true }),
-    del({ targets: exclude.map((e) => `${output.dir}/${e}`), hook: 'closeBundle' }),
 ];
 
 const external = [
