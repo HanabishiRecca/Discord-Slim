0b61e89e251fc286a3d0c5acd761b2c33a06a95c
Hanabishi <13597663+HanabishiRecca@users.noreply.github.com>
2021-10-07 18:26:17 +0500

    Update `Role` type

--- src/types.ts
+++ src/types.ts
@@ -601,8 +601,8 @@ export type Role = {
     name: string;
     color: number;
     hoist: boolean;
-    icon: string | null;
-    unicode_emoji: string | null;
+    icon?: string | null;
+    unicode_emoji?: string | null;
     position: number;
     permissions: string;
     managed: boolean;
