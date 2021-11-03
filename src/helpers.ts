64ccb88ce13dfd4d2063979ef0329427dd41ea39
Hanabishi <13597663+HanabishiRecca@users.noreply.github.com>
2021-11-03 19:19:19 +0500

    Update `SystemChannelFlags` enum

--- src/helpers.ts
+++ src/helpers.ts
@@ -227,6 +227,7 @@ export enum SystemChannelFlags {
     SUPPRESS_JOIN_NOTIFICATIONS = 1 << 0,
     SUPPRESS_PREMIUM_SUBSCRIPTIONS = 1 << 1,
     SUPPRESS_GUILD_REMINDER_NOTIFICATIONS = 1 << 2,
+    SUPPRESS_JOIN_NOTIFICATION_REPLIES = 1 << 3,
 }
 
 export enum GuildFeatures {
