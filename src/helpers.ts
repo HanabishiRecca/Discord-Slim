e210ed2ed887facb5fca55b4f99b4ac08c197f0e
Hanabishi <13597663+HanabishiRecca@users.noreply.github.com>
2021-11-03 19:12:01 +0500

    Update `ActivityFlags` enum

--- src/helpers.ts
+++ src/helpers.ts
@@ -333,6 +333,9 @@ export enum ActivityFlags {
     JOIN_REQUEST = 1 << 3,
     SYNC = 1 << 4,
     PLAY = 1 << 5,
+    PARTY_PRIVACY_FRIENDS = 1 << 6,
+    PARTY_PRIVACY_VOICE_CHANNEL = 1 << 7,
+    EMBEDDED = 1 << 8,
 }
 
 export enum ApplicationCommandOptionTypes {
