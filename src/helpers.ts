3689584cf101bf56e534a9256a9aae4c234aa8b7
Hanabishi <13597663+HanabishiRecca@users.noreply.github.com>
2021-11-03 19:17:16 +0500

    Update `UserFlags` enum

--- src/helpers.ts
+++ src/helpers.ts
@@ -292,20 +292,21 @@ export enum WidgetStyleOptions {
 }
 
 export enum UserFlags {
-    NO_FLAGS = 0,
-    DISCORD_EMPLOYEE = 1 << 0,
-    PARTNERED_SERVER_OWNER = 1 << 1,
-    HYPESQUAD_EVENTS = 1 << 2,
+    NONE = 0,
+    STAFF = 1 << 0,
+    PARTNER = 1 << 1,
+    HYPESQUAD = 1 << 2,
     BUG_HUNTER_LEVEL_1 = 1 << 3,
-    HOUSE_BRAVERY = 1 << 6,
-    HOUSE_BRILLIANCE = 1 << 7,
-    HOUSE_BALANCE = 1 << 8,
-    EARLY_SUPPORTER = 1 << 9,
-    TEAM_USER = 1 << 10,
+    HYPESQUAD_ONLINE_HOUSE_1 = 1 << 6,
+    HYPESQUAD_ONLINE_HOUSE_2 = 1 << 7,
+    HYPESQUAD_ONLINE_HOUSE_3 = 1 << 8,
+    PREMIUM_EARLY_SUPPORTER = 1 << 9,
+    TEAM_PSEUDO_USER = 1 << 10,
     BUG_HUNTER_LEVEL_2 = 1 << 14,
     VERIFIED_BOT = 1 << 16,
-    EARLY_VERIFIED_BOT_DEVELOPER = 1 << 17,
-    DISCORD_CERTIFIED_MODERATOR = 1 << 18,
+    VERIFIED_DEVELOPER = 1 << 17,
+    CERTIFIED_MODERATOR = 1 << 18,
+    BOT_HTTP_INTERACTIONS = 1 << 19,
 }
 
 export enum PremiumTypes {
