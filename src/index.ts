1f0e0c36f1342dc98727a14a3731547bfc7e2955
Hanabishi <13597663+HanabishiRecca@users.noreply.github.com>
2022-02-16 17:14:38 +0500

    Set `ES2020` modules

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,8 @@
 export { Client, ClientEvents } from './client.js';
 export { Authorization } from './request.js';
 export { Events } from './events.js';
-import * as Actions from './actions.js';
-import * as Helpers from './helpers.js';
-import type * as Types from './types';
-import * as Tools from './tools.js';
-export { Actions, Helpers, Types, Tools };
+export * as Actions from './actions.js';
+export * as Helpers from './helpers.js';
+import type * as Types from './types'; export type { Types };
+export * as Tools from './tools.js';
 export { Voice, VoiceEvents } from './voice.js';
